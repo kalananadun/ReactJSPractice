e2aa6b873541815f874f7d21226d9834085848ac
kalananadun <kalananadunn99@gmail.com>
2026-04-07 22:55:33 +0530

    Challenge 2 completed

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,94 +1,73 @@
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import AddItem from './components/AddItem';
-import SearchItem from './components/SearchItem';
-import { postItem } from '../functions/postItem';
-import {deleteItem} from '../functions/deleteItem';
-import {setCheckBoxItem, unsetCheckBoxItem} from "../functions/setCheckBoxItem";
+import React from 'react'
+import './challenge2.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+  Users: [],
+  Posts: [],
+  Comments: []
+});
+  const[dataRender, setDataRender] = useState([]);
+  useEffect(() => {
+    // get data in to the state here
+    const getData = async () => {
+      try{
+        const users = await fetch("https://jsonplaceholder.typicode.com/users");
+        const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
 
-const[item, setItem] = useState([]);
-const [search, setSearch] = useState("");
+        if(!users.ok || !posts.ok || !comments.ok) throw Error("Did not receive expected data");
 
-useEffect(()=>{
-  // item changes fetch and load item
-  const fetchItems = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/items");
-      if (!response.ok) throw Error("Did not receive expected data");
-      const listItems = await response.json();
-      setItem(listItems);
-    } catch (err) {
-      console.log(err.message);
+        const usersData = await users.json();
+        const postsData = await posts.json();
+        const commentsData = await comments.json();
+
+        setData({
+          Users: usersData,
+          Posts: postsData,
+          Comments: commentsData
+        }); 
+      }
+      catch(err){
+        console.log(err.message);
+      }
+
     }
+    (async () => await getData())();
   }
-
-  (async () => await fetchItems())();
-  
-},[])
-
-console.log(item);
-function searchItems(){
-  // take the serach function here onwards 
-  const searchResult = item.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
-  setItem(searchResult);
-}
-function setCheckbox(id) {
-      const updatedItems = item.map(i => {
-        if (i.id === id) {
-          return { ...i, isChecked: !i.isChecked };
-        }
-        return i;
-      });
-      
-
-      setItem(updatedItems);
-      if(updatedItems.find(i => i.id === id).isChecked){
-        (async ()=> await setCheckBoxItem(id))();
-      }
-      else{
-        (async ()=> await unsetCheckBoxItem(id))();
-      }
-    }
-function deleteTheItem(id) {
-      const updatedItems = item.filter(item => item.id !== id);
-
-      setItem(updatedItems);
-      (async ()=> await deleteItem(id))();
-    }
-function submitTheItem(e) {
-      e.preventDefault();
-
-      const id = item.length ? Number(item[item.length - 1].id )+ 1 : 1;
-      const name = e.target.elements.add_item.value;
-      const newItem = { id, name, isChecked: false };
-
-      const updatedItems = [...item, newItem];
-
-      setItem(updatedItems);
-
-      /* add to the back end here */
-      (async ()=> await postItem(newItem))();
-
-      e.target.elements.add_item.value = ""; 
-      console.log(item);
-}     
-
+  , [])
+  function onClickUser(e){
+    e.preventDefault();
+    setDataRender(data?.Users);
+    
+  }
+  function onClickPosts(e){
+    e.preventDefault();
+    setDataRender(data?.Posts);
+  }
+  function onClickComments(e){
+    e.preventDefault();
+    setDataRender(data?.Comments);
+  }
   return (
-    <div className="App">
-        <Header title={"Grocery List"}></Header>
-        <AddItem onSubmitItem={submitTheItem}></AddItem>
-        <SearchItem  searchQ={search} setSearch={setSearch}  />
-        <Content items={item?.filter((i) =>
-  i.name?.toLowerCase().includes(search?.toLowerCase())
-) || []} setCheckbox={setCheckbox} deleteTheItem={deleteTheItem} onSubmitItem={submitTheItem}></Content> 
-
-        <Footer length={item.length}></Footer>
+    <>
+    <div className='button-set'>
+      <button onClick={(e)=>onClickUser(e)}> Users</button>
+      <button onClick={(e)=>onClickPosts(e)}>Posts</button>
+      <button onClick={(e)=>onClickComments(e)}>Comments</button>
     </div>
+    <div className="content">
+      {/* content will be rendered here */}
+      {dataRender.length > 0 && dataRender.map((item, index) => (
+        <div key={index}>
+          {Object.entries(item).map(([key, value]) => (
+            <p key={key}><strong>{key}:</strong> {typeof value === 'object' ? JSON.stringify(value) : value}</p>
+          ))}
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
